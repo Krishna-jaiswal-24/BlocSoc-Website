478a9a9d841b2f821eaea6bea16318c10a8404e8
Krishna Jaiswal <krishnajaiswal2119@gmail.com>
2023-08-30 00:21:01 +0530

    added teams profile images

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,47 @@
-import BlocSoc from '../../resources/BlocSoc-Transparent.png';
-import './home.css';
-
+import BlocSoc from "../../resources/BlocSoc-Transparent.png";
+import "./home.css";
+import BlocSocLogo from '../../resources/Bloc.png'
+import Card1 from "../../components/Cards/Card1";
 const Home = () => {
   return (
     <div>
-        <div className='hero_section'>
-            <div className='container'>
-                <h1 className='header1'>Your Step to 
-                the <span className='color_blue'>Future</span>.</h1>
-                <p>Unlocking the powers of <span className='color_blue'>Blockchain</span>. Blockchain club of <span className='color_blue'> IIIT Dharwad</span></p>
-                <p>Create your Decentralised app today!</p>
-                <div className='hero_cta'>
-                    <button className='button_1'>Upcoming Events</button>
-                    <button className='button_2'>Explore BlocSoc</button>
-                </div>
-            </div>
-            <div>
-                <img className='hero_section_logo' src={BlocSoc} alt='logo'/>
-
-                
-            </div>
+      <div className="hero_section">
+        <div className="container">
+          <h1 className="header1">
+            Your Step to the <span className="color_blue">Future</span>.
+          </h1>
+          <p>
+            Unlocking the powers of{" "}
+            <span className="color_blue">Blockchain</span>. Blockchain club of{" "}
+            <span className="color_blue"> IIIT Dharwad</span>
+          </p>
+          <p>Create your Decentralised app today!</p>
+          <div className="hero_cta">
+            <button className="button_1">Upcoming Events</button>
+            <button className="button_2">Explore BlocSoc</button>
+          </div>
+        </div>
+        <div>
+          <img className="hero_section_logo" src={BlocSoc} alt="logo" />
+        </div>
+      </div>
+      <div>
+          <div className="blog_header">
+            <h1 className="header1 color_blue">
+              <img className="blog_logo" src={BlocSocLogo} alt="logo" /> Blogs
+            </h1>
+          </div>
+          <div className="blogs_container_row">
+            <Card1 className="blogs" />
+            <Card1 className="blogs" />
+            <Card1 className="blogs" />
+            <Card1 className="blogs" />
+            <Card1 className="blogs" />
+            <Card1 className="blogs" />
+          </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import BlocSoc from "../../resources/Bloc.png";
 import AshitoshSable from "../../resources/Ashitosh_Sable_21bcs096.png";
+import Ajay from "../../resources/Ajay.jpg";
+import Priyesh from "../../resources/Priyesh.jpg";
+import Shreyansh from "../../resources/Shreyansh.jpg";
+import Krishna from "../../resources/Krishna.jpg"
+
 import "./teams.css";
 const Team = () => {
+  const profiles = [{
+    name: 'Ajay Bhakar',
+    img: Ajay,
+    post: "President",
+    key: 1,
+  }, {
+    name: 'Ashitosh Sable',
+    img: AshitoshSable,
+    post: "Content Lead",
+    key: 2,
+  },{
+    name: 'Priyesh Gupta',
+    img: Priyesh,
+    post: "Frontend Lead",
+    key: 3,
+  },{
+    name: 'Shreyansh Tiwari',
+    img: Shreyansh,
+    post: "Backend Lead",
+    key: 4,
+  },{
+    name: 'Nupur Sangwai',
+    img: Priyesh,
+    post: "Research Lead",
+    key: 5,
+  },{
+    name: 'Krishna Jaiswal',
+    img: Krishna,
+    post: "Frontend Member",
+    key: 6,
+  },{
+    name: 'Krishna Jaiswal',
+    img: Krishna,
+    post: "Frontend Member",
+    key: 6,
+  },{
+    name: 'Krishna Jaiswal',
+    img: Krishna,
+    post: "Frontend Member",
+    key: 6,
+  },{
+    name: 'Krishna Jaiswal',
+    img: Krishna,
+    post: "Frontend Member",
+    key: 6,
+  }]
   return (
     <div>
       <div className="teams_container">
@@ -34,36 +85,15 @@ const Team = () => {
       <div className="teams_container">
         <h1 className="header1">Team</h1>
         <div className="teams_profile">
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
-          <div className="individual_profiles">
-            <img src={AshitoshSable} alt="profile-pic" />
-            <p className="header3">Ashitosh Sable</p>
-            <p className="color_blue header2">Content Lead</p>
-          </div>
+          {profiles.map((profile) => {
+            return (
+              <div className="individual_profiles">
+                <img src={profile.img} alt="profile-pic" className="profile_images" />
+                <p className="header3">{profile.name} </p>
+                <p className="color_blue header4">{profile.post}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
